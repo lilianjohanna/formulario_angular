9d8bdb43539c60f1c74963e731ae9044a19251a3
lilianjohanna <61803904+lilianjohanna@users.noreply.github.com>
2024-03-21 17:05:39 +0000

    ...

---
+++ formulario/formulario_mysql/src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppComponent } from './app.component';
+import { NombreUsuarioComponent } from './nombre-usuario/nombre-usuario.component';
+import { DireccionUsuarioComponent } from './direccion-usuario/direccion-usuario.component';
+import { TelefonoUsuarioComponent } from './telefono-usuario/telefono-usuario.component';
+import { CorreoUsuarioComponent } from './correo-usuario/correo-usuario.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NombreUsuarioComponent,
+    DireccionUsuarioComponent,
+    TelefonoUsuarioComponent,
+    CorreoUsuarioComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
