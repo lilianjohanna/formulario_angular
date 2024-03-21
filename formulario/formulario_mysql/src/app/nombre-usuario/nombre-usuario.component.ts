7947a48a7e1967f5f2d6fd6d8b5647b6e73bb050
lilianjohanna <61803904+lilianjohanna@users.noreply.github.com>
2024-03-21 19:55:35 +0000

    ...

---
+++ formulario/formulario_mysql/src/app/nombre-usuario/nombre-usuario.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nombre-usuario',
+  standalone: true,
+  imports: [],
+  templateUrl: './nombre-usuario.component.html',
+  styleUrl: './nombre-usuario.component.css'
+})
+export class NombreUsuarioComponent {
+
+}
