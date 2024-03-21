0fe673cf454a003f338bffa618653af17b652c3a
lilianjohanna <61803904+lilianjohanna@users.noreply.github.com>
2024-03-21 20:45:47 +0000

    ...

--- formulario/formulario_mysql/src/app/app.module.ts
+++ formulario/formulario_mysql/src/app/app.module.ts
@@ -4,10 +4,11 @@ import { NombreUsuarioComponent } from './nombre-usuario/nombre-usuario.componen
 import { DireccionUsuarioComponent } from './direccion-usuario/direccion-usuario.component';
 import { TelefonoUsuarioComponent } from './telefono-usuario/telefono-usuario.component';
 import { CorreoUsuarioComponent } from './correo-usuario/correo-usuario.component';
+import { EnviarComponent } from './enviar/enviar.component';
 
 
 @NgModule({
-  declarations: [NombreUsuarioComponent,DireccionUsuarioComponent,TelefonoUsuarioComponent,CorreoUsuarioComponent],
+  declarations: [NombreUsuarioComponent,DireccionUsuarioComponent,TelefonoUsuarioComponent,CorreoUsuarioComponent,EnviarComponent],
   imports: [
     CommonModule
   ]
---
+++ formulario/formulario_mysql/src/app/enviar/enviar.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-enviar',
+  standalone: true,
+  imports: [],
+  templateUrl: './enviar.component.html',
+  styleUrl: './enviar.component.css'
+})
+export class EnviarComponent {
+
+}
