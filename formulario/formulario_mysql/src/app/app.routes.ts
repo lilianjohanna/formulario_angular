9fa1ff4d3c79145ee33aa0cf9c1d31b61cd58926
lilianjohanna <61803904+lilianjohanna@users.noreply.github.com>
2024-03-21 17:34:46 +0000

    ...

--- formulario/formulario_mysql/src/app/app.routes.ts
+++ formulario/formulario_mysql/src/app/app.routes.ts
@@ -1,3 +1,19 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { NombreUsuarioComponent } from './nombre-usuario/nombre-usuario.component';
+import { DireccionUsuarioComponent } from './direccion-usuario/direccion-usuario.component';
+import { TelefonoUsuarioComponent } from './telefono-usuario/telefono-usuario.component';
+import { CorreoUsuarioComponent } from './correo-usuario/correo-usuario.component';
 
-export const routes: Routes = [];
+const routes: Routes = [
+  { path: './nombre-usuario', component: NombreUsuarioComponent },
+  { path: './direccion-usuario', component: DireccionUsuarioComponent },
+  { path: './telefono-usuario', component: TelefonoUsuarioComponent },
+  { path: './correo-usuario', component: CorreoUsuarioComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
