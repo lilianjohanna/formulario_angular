e13ea00ef52a3b115c0171a279f8003d4d81f4ed
lilianjohanna <61803904+lilianjohanna@users.noreply.github.com>
2024-03-21 20:42:13 +0000

    ...

--- formulario/formulario_mysql/src/app/app.component.ts
+++ formulario/formulario_mysql/src/app/app.component.ts
@@ -14,4 +14,18 @@ import { CorreoUsuarioComponent } from './correo-usuario/correo-usuario.componen
 })
 export class AppComponent {
   title = 'formulario_mysql';
+  formData = {
+    NombreUsuarioComponent: '',
+    DireccionUsuarioComponent: '',
+    TelefonoUsuarioComponent: '',
+    CorreoUsuarioComponent: ''
+  };
+  submitForm() {
+    if (this.formData.NombreUsuarioComponent && this.formData.DireccionUsuarioComponent && this.formData.TelefonoUsuarioComponent && this.formData.CorreoUsuarioComponent) {
+      console.log('Formulario enviado correctamente:', this.formData);
+      // Aquí puedes enviar los datos a tu servidor o realizar cualquier acción necesaria
+    } else {
+      console.error('No se pueden dejar campos en blanco');
+    }
+  }
 }
