7083a67575482c2f9d12f7a55cade2684527ccd0
lilianjohanna <61803904+lilianjohanna@users.noreply.github.com>
2024-03-21 17:49:22 +0000

    ...

--- formulario/formulario_mysql/src/app/app.component.ts
+++ formulario/formulario_mysql/src/app/app.component.ts
@@ -1,13 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NombreUsuarioComponent } from './nombre-usuario/nombre-usuario.component';
+import { DireccionUsuarioComponent } from './direccion-usuario/direccion-usuario.component';
+import { TelefonoUsuarioComponent } from './telefono-usuario/telefono-usuario.component';
+import { CorreoUsuarioComponent } from './correo-usuario/correo-usuario.component';
 
 @Component({
-  selector: 'app-component',
+  selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'formulario_mysql';
+export class AppComponent implements OnInit {
+  constructor(private NombreUsuarioComponent: NombreUsuarioComponent, private DireccionUsuarioComponent: DireccionUsuarioComponent, private TelefonoUsuarioComponent: TelefonoUsuarioComponent, private CorreoUsuarioComponent: CorreoUsuarioComponent) { }  
+  ngOnInit() {
+    // Aquí puedes utilizar el componente de ejemplo
+  }
 }
