91488a3f77b8d2c8e93df0c2b9f8dab130ecc71e
lilianjohanna <61803904+lilianjohanna@users.noreply.github.com>
2024-03-21 20:14:42 +0000

    ...

--- formulario/formulario_mysql/src/app/app.component.ts
+++ formulario/formulario_mysql/src/app/app.component.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NombreUsuarioComponent } from './nombre-usuario/nombre-usuario.component';
+import { DireccionUsuarioComponent } from './direccion-usuario/direccion-usuario.component';
+import { TelefonoUsuarioComponent } from './telefono-usuario/telefono-usuario.component';
+import { CorreoUsuarioComponent } from './correo-usuario/correo-usuario.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
+  imports: [RouterOutlet,NombreUsuarioComponent,DireccionUsuarioComponent,TelefonoUsuarioComponent,CorreoUsuarioComponent],
+  template: `<form>
+  <app-nombre-usuario></app-nombre-usuario>
+  <app-direccion-usuario></app-direccion-usuario>
+  <app-telefono-usuario></app-telefono-usuario>
+  <app-correo-usuario></app-correo-usuario>
+  </form>`,
   styleUrl: './app.component.css'
 })
 export class AppComponent {
--- formulario/formulario_mysql/src/app/app.module.ts
+++ formulario/formulario_mysql/src/app/app.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { NombreUsuarioComponent } from './nombre-usuario/nombre-usuario.component';
+import { DireccionUsuarioComponent } from './direccion-usuario/direccion-usuario.component';
+import { TelefonoUsuarioComponent } from './telefono-usuario/telefono-usuario.component';
+import { CorreoUsuarioComponent } from './correo-usuario/correo-usuario.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [NombreUsuarioComponent,DireccionUsuarioComponent,TelefonoUsuarioComponent,CorreoUsuarioComponent],
   imports: [
     CommonModule
   ]
