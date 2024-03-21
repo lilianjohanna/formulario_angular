b6aa0bcefbbc6970aa80cd94c87859571752d49a
lilianjohanna <61803904+lilianjohanna@users.noreply.github.com>
2024-03-21 20:47:29 +0000

    ...

--- formulario/formulario_mysql/src/app/app.component.ts
+++ formulario/formulario_mysql/src/app/app.component.ts
@@ -4,11 +4,12 @@ import { NombreUsuarioComponent } from './nombre-usuario/nombre-usuario.componen
 import { DireccionUsuarioComponent } from './direccion-usuario/direccion-usuario.component';
 import { TelefonoUsuarioComponent } from './telefono-usuario/telefono-usuario.component';
 import { CorreoUsuarioComponent } from './correo-usuario/correo-usuario.component';
+import { EnviarComponent } from './enviar/enviar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,NombreUsuarioComponent,DireccionUsuarioComponent,TelefonoUsuarioComponent,CorreoUsuarioComponent],
+  imports: [RouterOutlet,NombreUsuarioComponent,DireccionUsuarioComponent,TelefonoUsuarioComponent,CorreoUsuarioComponent,EnviarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
