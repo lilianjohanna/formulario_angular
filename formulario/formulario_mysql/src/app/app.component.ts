5eedca47125d56f41ba4d96b838e4140e9d81fcf
lilianjohanna <61803904+lilianjohanna@users.noreply.github.com>
2024-03-21 21:40:00 +0000

    ...

--- formulario/formulario_mysql/src/app/app.component.ts
+++ formulario/formulario_mysql/src/app/app.component.ts
@@ -11,7 +11,7 @@ import { NgModel } from '@angular/forms';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,NombreUsuarioComponent,DireccionUsuarioComponent,TelefonoUsuarioComponent,CorreoUsuarioComponent,EnviarComponent,NgIf,NgModel],
+  imports: [RouterOutlet,NombreUsuarioComponent,DireccionUsuarioComponent,TelefonoUsuarioComponent,CorreoUsuarioComponent,EnviarComponent,NgIf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
