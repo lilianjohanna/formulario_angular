935bacc0d940fbc5cbc0e0d8acf650df39f61325
lilianjohanna <61803904+lilianjohanna@users.noreply.github.com>
2024-03-22 17:58:58 +0000

    ...

--- formulario/formulario_mysql/src/app/app.component.ts
+++ formulario/formulario_mysql/src/app/app.component.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TextoComponent } from './texto/texto.component';
 import { NumeroComponent } from './numero/numero.component';
 import { BotonComponent } from './boton/boton.component';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -11,6 +12,20 @@ import { BotonComponent } from './boton/boton.component';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+  formulario!: FormGroup;
+  ngOnInit(): void {
+    this.formulario = new FormGroup({
+      TextoComponent: new FormControl('', [Validators.required]),
+      NumeroComponent: new FormControl('', [Validators.required, Validators.minLength(8)]),
+    });
+  }
+  enviarFormulario(): void {
+    if (this.formulario.valid) {
+      console.log('Formulario enviado:', this.formulario.value);
+    } else {
+      console.log('Formulario no válido');
+    }
+  }
   title = 'formulario_mysql';
 }
