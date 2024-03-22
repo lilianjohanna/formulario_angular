bac4e8c73057f4290aaadc83eaafc33662ac6f1f
lilianjohanna <61803904+lilianjohanna@users.noreply.github.com>
2024-03-22 18:07:31 +0000

    ...

---
+++ formulario/formulario_mysql/src/app/app.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule, NgIf } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+
+
+@NgModule({
+  declarations: [NgIf],
+  imports: [
+    CommonModule,
+    FormsModule
+  ]
+})
+export class AppModule { }
