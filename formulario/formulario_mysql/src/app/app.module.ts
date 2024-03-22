0915baff09d437df3665eff6bf8ddbf59ca4d5c5
lilianjohanna <61803904+lilianjohanna@users.noreply.github.com>
2024-03-22 01:59:44 +0000

    ...

--- formulario/formulario_mysql/src/app/app.module.ts
+++ formulario/formulario_mysql/src/app/app.module.ts
@@ -1,12 +1,23 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
-
+import { AppComponent } from './app.component';
+import { UserFormComponent } from './user-form/user-form.component';
+import { CountryDropdownComponent } from './country-dropdown/country-dropdown.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    AppComponent,
+    UserFormComponent,
+    CountryDropdownComponent
+  ],
   imports: [
-    CommonModule
-  ]
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ formulario/formulario_mysql/src/app/country-dropdown/country-dropdown.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-country-dropdown',
+  standalone: true,
+  imports: [],
+  templateUrl: './country-dropdown.component.html',
+  styleUrl: './country-dropdown.component.css'
+})
+export class CountryDropdownComponent {
+
+}
