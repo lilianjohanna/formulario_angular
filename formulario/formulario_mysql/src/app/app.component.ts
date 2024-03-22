f4c1d9b44e152746d2802c01b44b97708612de99
lilianjohanna <61803904+lilianjohanna@users.noreply.github.com>
2024-03-22 17:18:38 +0000

    ...

--- formulario/formulario_mysql/src/app/app.component.ts
+++ formulario/formulario_mysql/src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NumeroComponent } from './numero/numero.component';
 import { TextoComponent } from './texto/texto.component';
+import { BotonComponent } from './boton/boton.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,NumeroComponent,TextoComponent],
+  imports: [RouterOutlet,NumeroComponent,TextoComponent,BotonComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
