9eb9f8f8bad732799864d048a8fdf8472d124d71
lilianjohanna <61803904+lilianjohanna@users.noreply.github.com>
2024-03-22 02:07:59 +0000

    ...

--- formulario/formulario_mysql/src/app/app.component.ts
+++ formulario/formulario_mysql/src/app/app.component.ts
@@ -1,12 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { UserFormComponent } from './user-form/user-form.component';
-import { CountryDropdownComponent } from './country-dropdown/country-dropdown.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,UserFormComponent,CountryDropdownComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
