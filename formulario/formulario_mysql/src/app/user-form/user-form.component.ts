2565f7beaf65fc8412c01ae7ef006c0264dfe0b3
lilianjohanna <61803904+lilianjohanna@users.noreply.github.com>
2024-03-22 02:55:51 +0000

    ...

--- formulario/formulario_mysql/src/app/user-form/user-form.component.ts
+++ formulario/formulario_mysql/src/app/user-form/user-form.component.ts
@@ -7,7 +7,7 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./user-form.component.css']
 })
 export class UserFormComponent implements OnInit {
-  userForm!: FormGroup;
+  userForm: FormGroup;
 
   constructor(private formBuilder: FormBuilder) { }
 
