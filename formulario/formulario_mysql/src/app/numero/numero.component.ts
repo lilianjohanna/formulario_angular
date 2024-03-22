ebbab627afa8766c0634011f5cd881c648feb6b5
lilianjohanna <61803904+lilianjohanna@users.noreply.github.com>
2024-03-22 16:25:48 +0000

    ...

---
+++ formulario/formulario_mysql/src/app/numero/numero.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-numero',
+  standalone: true,
+  imports: [],
+  templateUrl: './numero.component.html',
+  styleUrl: './numero.component.css'
+})
+export class NumeroComponent {
+
+}
