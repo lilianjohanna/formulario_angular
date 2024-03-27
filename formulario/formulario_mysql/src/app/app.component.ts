1900f6b1005efb0243123ba49940c58a095b7ef5
lilianjohanna <61803904+lilianjohanna@users.noreply.github.com>
2024-03-27 23:39:51 +0000

    ...

--- formulario/formulario_mysql/src/app/app.component.ts
+++ formulario/formulario_mysql/src/app/app.component.ts
@@ -15,8 +15,8 @@ import { ReactiveFormsModule } from '@angular/forms';
 })
 export class AppComponent implements OnInit{
   formulario!: FormGroup;
-  texto?: any;
-  numero?: any;
+  texto="texto";
+  numero=0;
   constructor()
   {
     this.formulario = new FormGroup({
