418bb459747221aa913832c07c017f2c79cc2889
lilianjohanna <61803904+lilianjohanna@users.noreply.github.com>
2024-03-28 04:03:47 +0000

    ...

--- formulario/formulario_mysql/src/app/app.component.ts
+++ formulario/formulario_mysql/src/app/app.component.ts
@@ -15,6 +15,8 @@ import { ReactiveFormsModule } from '@angular/forms';
 })
 export class AppComponent implements OnInit{
   formulario!: FormGroup;
+  texto: any;
+  numero: any;
   ngOnInit(): void {
     this.formulario = new FormGroup({
       texto: new FormControl('',Validators.required),
@@ -22,7 +24,7 @@ export class AppComponent implements OnInit{
     });    
   }
   enviarFormulario(): void {
-    if(this.formulario.valid) {
+    if(this.texto!='' || this.numero!='' || this.numero==0) {
       alert(this.formulario.valid);
       //console.log('Formulario enviado:', this.formulario.value);
     } else {
