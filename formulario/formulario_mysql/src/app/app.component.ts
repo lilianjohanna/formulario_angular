143e4a5c034a9b287f2be220b52275f531feb072
lilianjohanna <61803904+lilianjohanna@users.noreply.github.com>
2024-04-03 17:58:20 +0000

    ...

--- formulario/formulario_mysql/src/app/app.component.ts
+++ formulario/formulario_mysql/src/app/app.component.ts
@@ -1,13 +1,26 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { DataService } from './app.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'formulario_mysql';
+  constructor(private dataService: DataService) {}
+
+  onSubmit(formData: any) {
+    this.dataService.sendData(formData).subscribe(
+      (response) => {
+        console.log(response);
+      },
+      (error) => {
+        console.error(error);
+      },
+    );
+  }
 }
